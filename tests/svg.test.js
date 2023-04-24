6c17ed935f2d571063214b257b2316a0b3d34b0f
Jesse Emerson <jesseemerson7@gmail.com>
2023-04-24 19:30:19 -0400

    added yAxis variable to svg and shape classes fixed tests

--- tests/svg.test.js
+++ tests/svg.test.js
@@ -2,7 +2,7 @@ const SVG = require("../lib/svg");
 
 describe("SVG", () => {
   test("should return rendered circle SVG", () => {
-    const testSvg = new SVG("svg","black",`<circle cx="150" cy="100" r="80" fill="blue" />`);
+    const testSvg = new SVG("svg","black",`<circle cx="150" cy="100" r="80" fill="blue" />`,125);
     const expectedString=`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
     <circle cx="150" cy="100" r="80" fill="blue" />
     <text x="150" y="125" font-size="60" text-anchor="middle" fill="black">svg</text>
@@ -11,7 +11,7 @@ describe("SVG", () => {
   });
 
   test("should return rendered Square SVG", () => {
-    const testSvg = new SVG("svg","black",`<rect x="90" y="40" width="120" height="120" fill="blue" />`);
+    const testSvg = new SVG("svg","black",`<rect x="90" y="40" width="120" height="120" fill="blue" />`,125);
     const expectedString=`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
     <rect x="90" y="40" width="120" height="120" fill="blue" />
     <text x="150" y="125" font-size="60" text-anchor="middle" fill="black">svg</text>
@@ -20,10 +20,10 @@ describe("SVG", () => {
   });
 
   test("should return rendered Triangle SVG", () => {
-    const testSvg = new SVG("svg","black",`<polygon points="150, 18 244, 182 56, 182" fill="blue" />`);
+    const testSvg = new SVG("svg","black",`<polygon points="150, 18 244, 182 56, 182" fill="blue" />`,150);
     const expectedString=`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
     <polygon points="150, 18 244, 182 56, 182" fill="blue" />
-    <text x="150" y="125" font-size="60" text-anchor="middle" fill="black">svg</text>
+    <text x="150" y="150" font-size="60" text-anchor="middle" fill="black">svg</text>
   </svg>`;
     expect(testSvg.render()).toEqual(expectedString);
   });
